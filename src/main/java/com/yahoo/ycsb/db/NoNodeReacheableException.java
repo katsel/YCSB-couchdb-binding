843bb145f6255ce6f622f84bdde971c0d80a0b47
katsel <katsel@users.noreply.github.com>
2016-11-29 14:03:19 +0100

    Fix checkstyle warnings: minor changes to NoNodeReacheableException

--- src/main/java/com/yahoo/ycsb/db/NoNodeReacheableException.java
+++ src/main/java/com/yahoo/ycsb/db/NoNodeReacheableException.java
@@ -1,5 +1,8 @@
-package couchdb;
+package com.yahoo.ycsb.db;
 
+/**
+ * Exception denoting that no node is reachable.
+ */
 public class NoNodeReacheableException extends RuntimeException {
 
   private static final long serialVersionUID = 1L;
